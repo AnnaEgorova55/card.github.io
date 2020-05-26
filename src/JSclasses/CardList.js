9a032f3f4a5d8243c79c16cbfe9e2b59363e4613
anna egorova <anya.eg2015@yandex.ru>
2020-05-26 22:25:42 +0300

    13 commit

---
+++ src/JSclasses/CardList.js
@@ -0,0 +1,39 @@
+export default class CardList {
+  constructor(container, api, renderCards, openZoom, formAddCard, myName, myLink) {
+    this.container = container;
+    this.api = api;
+    this.renderCards = renderCards;
+    this.openZoom = openZoom;
+    this.formAddCard = formAddCard;
+    this.addCard = this.addCard.bind(this);
+    this.myName = myName;
+    this.myLink = myLink;
+    this.setEventListener();
+  }
+
+  addCard(obj) {
+    const newCard = this.renderCards(obj.name, obj.link, this.openZoom);
+    this.container.appendChild(newCard);
+  }
+
+  render() {
+    this.api.getInitialCards().then((res) => {
+      res.forEach((elem) => {
+        this.addCard(elem);
+      });
+    });
+  }
+
+  setEventListener() {
+    this.formAddCard.addEventListener('submit', (event) => {
+      const obj = {
+        name: this.myName.value,
+        link: this.myLink.value
+      };
+      event.preventDefault();
+      this.addCard(obj);
+      this.formAddCard.reset();
+    });
+  }
+
+}
---
+++ src/JSclasses/FormValidator.js
@@ -0,0 +1,41 @@
+export default class FormValidator {
+  constructor(form, words, button) {
+    this.form = form;
+    this.words = words;
+    this.button = button;
+    this.setEventListeners();
+  }
+
+  checkInputValidity(event) {
+    const error = document.querySelector(`#error-${event.target.id}`);
+    error.textContent = '';
+    if (event.target.validity.valueMissing) {
+      error.textContent = this.words.validationFilling;
+      return;
+    }
+    if (event.target.validity.tooShort || event.target.validity.tooLong) {
+      error.textContent = this.words.validationLenght;
+      return;
+    }
+
+    this.setSubmitButtonState(event);
+  }
+
+  setSubmitButtonState(event) {
+    if (event.target.validity.valid) {
+      this.button.classList.add('popup__button_active');
+      this.button.removeAttribute('disabled');
+    } else {
+      this.button.classList.remove('popup__button_active');
+      this.button.setAttribute('disabled', true);
+    }
+  }
+
+  setEventListeners() {
+    Array.from(this.form.querySelectorAll('.popup__input')).forEach((elem) => {
+      elem.addEventListener('input', (event) => {
+        this.checkInputValidity(event);
+      });
+    });
+  }
+}
